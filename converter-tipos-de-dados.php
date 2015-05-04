6df9330bfcd0468915642f00191dd18cadadf8b4
Alvaro Cavalcante Costa <make.it.alv@gmail.com>
2015-05-04 15:11:36 -0300

    adicionado arquivo converter-tipos-de-dados.php

---
+++ converter-tipos-de-dados.php
@@ -0,0 +1,17 @@
+<?php
+
+$variavel = 'Alvaro Costa';
+$variavel = '123numero123';
+$variavel = 'numero123';
+
+echo (int)$variavel;
+
+/*
+ * (int)
+ * (float)
+ * (bool)
+ * (string)
+ * (array)
+ * (object)
+ */
+
