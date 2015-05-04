910ff76da10574fbdaa078f150ef6b0b13751cb9
Alvaro Cavalcante Costa <make.it.alv@gmail.com>
2015-05-04 16:06:34 -0300

    adicionado o arquivo constantes.php

---
+++ constantes.php
@@ -0,0 +1,20 @@
+<?php
+
+define('CONSTANTE', 'Oi, mundo.', true);
+
+echo CONSTANTE;
+echo constante;
+
+echo '<br>';
+echo '<hr>';
+echo '<br>';
+
+echo __FILE__;
+echo ':';
+echo __LINE__;
+
+/*
+ * __FUNCTION__
+ * __CLASS__
+ * __METHOD__
+ */
