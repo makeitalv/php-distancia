34e41d31f00948ceae4295b4f56d37110082d68d
Alvaro Cavalcante Costa <make.it.alv@gmail.com>
2015-05-04 18:14:08 -0300

    adicionando comentarios.php com sintaxe de função

---
+++ comentarios.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Esse
+ */
+
+/*
+Esse
+ */
+
+
+/**
+ * Comentario de funcao padrao com 2 asteriscos.
+ * @param type $var1
+ * @param type $var2
+ * @return boolean
+ */
+function nome_da_funcao ($var1, $var2){
+    return true;
+}
