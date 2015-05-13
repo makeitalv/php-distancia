a19d4087f6f1e6df684373e2a8f81befe2fb1c35
Alvaro Costa <make.it.alv@gmail.com>
2015-05-13 14:45:24 -0300

    adicionando arquivo if-operadores-logicos.php

---
+++ if-operadores-logicos.php
@@ -0,0 +1,25 @@
+<?php
+
+if(){
+    
+    
+}
+
+elseif{
+    
+}
+
+else{
+    
+   
+}
+
+/*
+ * Operadores Lógicos Exemplo 	Nome 	Resultado
+ * $a and $b 	E 	Verdadeiro (TRUE) se tanto $a quanto $b são verdadeiros.
+ * $a or $b 	OU 	Verdadeiro se $a ou $b são verdadeiros.
+ * $a xor $b 	XOR 	Verdadeiro se $a ou $b são verdadeiros, mas não ambos.
+ * ! $a 	NÃO 	Verdadeiro se $a não é verdadeiro.
+ * $a && $b 	E 	Verdadeiro se tanto $a quanto $b são verdadeiros.
+ * $a || $b 	OU 	Verdadeiro se $a ou $b são verdadeiros.
+ */
